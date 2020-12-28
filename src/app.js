8644119384000b444f6ed14863a1f959f69ad125
thomas <hjh8684@gmail.com>
2020-12-28 20:41:43 +0900

    add set read

--- src/app.js
+++ src/app.js
@@ -112,7 +112,7 @@ io.of('/test').on('connection', (socket) => {
   socket.on('setRead', (msg) => {
     setDomain(msg.domain);
     console.log('set read ' + msg);
-    const qry = `UPDATE chat SET read = 1 WHERE chat_list_id = ? and sender_type = ?`;
+    const qry = `UPDATE chats SET read = 1 WHERE chat_list_id = ? and sender_type = ?`;
 
     const condition = [msg.chat_list_id, msg.sender_type];
 
